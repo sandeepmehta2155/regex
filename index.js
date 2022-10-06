2d52e39f246bf4c1fb5c8e05fb528ee54dd48363
Sandeep Mehta <66156650+sandeepmehta2155@users.noreply.github.com>
2022-10-06 15:27:56 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const regex = /^[6-9]\d{9}$/gm;
+
+const regexEmail = /^([a-zA-Z0-9\._-]+)@([a-zA-Z0-9]+)\.([a-zA-Z0-9])/;
+
+let regexEmailTwo =
+  /^([a-z0-9\.-]+)@([a-z0-9-]+)\.([a-z]{2,20})(.[a-z]{2,10})?$/;
+// console.log(regex.test("7041818312"));
+console.log(regexEmailTwo.test("saneep@hotmail.co.in"));
+console.log(regexEmailTwo.test("saneep@gmail.com"));
